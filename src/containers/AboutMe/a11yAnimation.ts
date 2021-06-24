c8e11f93801d027407f4e09facb630a4358e91ea
Caleb Taylor <aquaductape@gmail.com>
2021-06-24 12:55:58 -0700

    update

--- src/containers/AboutMe/a11yAnimation.ts
+++ src/containers/AboutMe/a11yAnimation.ts
@@ -101,6 +101,7 @@ export const a11yAnimation = ({
 
   // cardContent0. = "url(#a11y-protanopia)";
   cardContent0.setAttribute("filter", "url(#a11y-protanopia)");
+  cardContent0.style.transform = "translate(0, 0)";
 
   const start = () => {
     mTimeline.scene(
